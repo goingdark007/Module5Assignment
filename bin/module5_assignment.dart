ce16a4d89562150d8b187b3da5696d7232daea84
Nazmul <nhasanr18@gmail.com>
2025-09-10 12:22:55 +0600

    Solved the first question

--- bin/module5_assignment.dart
+++ bin/module5_assignment.dart
@@ -1,4 +1,46 @@
+class Book {
+
+  /// properties
+  final String title;
+  final String author;
+  double price;
+
+  /// Constructor
+  Book({required this.title, required this.author, required this.price});
+
+  /// Method
+  void discountedPrice (int discount) {
+    // calculating price after discount
+    price -= (price * (discount / 100)).round();
+  }
+
+  /// Method to show title and authors name
+  void showDetails() {
+    print('Book name :- $title');
+    print('Books author :- $author');
+  }
+
+}
+
+
 void main () {
 
+  /// Creating instance or object of the Book class
+  Book book1 = Book(title: 'Science Fiction Samagra', author: 'Humayun Ahmed', price: 860);
+  Book book2 = Book(title: 'Foundation', author: 'Isaac Asimov', price: 259);
+
+  // book1 details
+  book1.showDetails();
+  print('Before discount price :- ${book1.price}');
+  // 10% discount
+  book1.discountedPrice(10);
+  print('After discount price :- ${book1.price}\n');
+
+  // book2 details
+  book2.showDetails();
+  print('Before discount price :- ${book2.price}');
+  // 5% discount
+  book2.discountedPrice(5);
+  print('After discount price :- ${book2.price}');
 
 }
